234d7d3ebfef2c230870783a2ba0d43b9e3d1188
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-02-03 18:04:33 +0100

    code for a billion dollar buiness

---
+++ billionDollarApp.js
@@ -0,0 +1,12 @@
+const billionDollarApp = (ideas) => {
+  let futureCurrency = ideas;
+
+  // input 100 ideas
+  // 5 are sticky
+  // 1 generates a billion dollar
+
+  return 1000000000;
+};
+
+const result = billionDollarApp("money");
+console.log(result);
